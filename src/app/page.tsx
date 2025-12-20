4d54ea6e52d70126e8c7c7efaf590d36dd61d179
k4rtikay <katt1bh4i@gmail.com>
2025-12-20 17:30:08 +0000

    feat: setup landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,36 @@
 export default async function HomePage() {
 
   return (
-    <main>
-      <h1>Welcome to the Home Page</h1>
-      <p>This is the main landing page of the application.</p>
-    </main>
-  );  
+    <div className="min-h-screen flex flex-col">
+      <header className="flex justify-between px-4 py-2">
+        <h1>AI Folio</h1>
+        <span className="flex gap-2">
+          <button>Github</button>
+          <button>Login</button>
+        </span>
+      </header>
+      <main className="flex flex-col items-center p-24">
+        <div className="text-center">
+          <h1 className="text-5xl font-extrabold tracking-tight">
+            Build Your Vision.
+          </h1>
+          <p className="mt-2 text-lg leading-8">
+            Turn your github into your brand. Choose from many fun templates.
+          </p>
+        </div>
+        <form action="" className="mt-6 flex gap-2">
+          <input
+            type="text"
+            name="username"
+            placeholder="Enter your github username..."
+            className="rounded-md border border-gray-300 px-3 py-2 w-60"
+          />
+          <button type="submit" className="rounded-md bg-black px-3 py-2 text-white">Create</button>
+        </form>
+      </main>
+      <footer>
+        
+      </footer>
+    </div>
+  );
 }
