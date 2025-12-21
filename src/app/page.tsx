4befc98946c178fde58abc46e3552fcf7f25646b
k4rtikay <katt1bh4i@gmail.com>
2025-12-21 07:22:47 +0000

    chore: change some copy on landing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ export default async function HomePage() {
   return (
     <div className="min-h-screen flex flex-col justify-between">
       <header className="flex justify-between px-4 py-2">
-        <h1>AI Folio</h1>
+        <h1>GitExhibit</h1>
         <span className="flex gap-2">
           <button>Github</button>
           <button>Login</button>
@@ -48,10 +48,10 @@ export default async function HomePage() {
             </div>
 
             <div className="flex flex-col gap-2">
-              <h3 className="text-sm font-semibold text-gray-900">Company</h3>
-              <a href="#" className="text-sm text-gray-600 hover:text-gray-900">About</a>
-              <a href="#" className="text-sm text-gray-600 hover:text-gray-900">Careers</a>
-              <a href="#" className="text-sm text-gray-600 hover:text-gray-900">Press</a>
+              <h3 className="text-sm font-semibold text-gray-900">Contact</h3>
+              <a href="#" className="text-sm text-gray-600 hover:text-gray-900">Support</a>
+              <a href="#" className="text-sm text-gray-600 hover:text-gray-900">Contribute</a>
+              <a href="#" className="text-sm text-gray-600 hover:text-gray-900">Feedback</a>
             </div>
 
             <form action="" className="flex flex-col gap-2">
@@ -66,8 +66,8 @@ export default async function HomePage() {
           </div>
         </div>
         <h1
-        className="text-center text-[80px] md:text-[200px] font-bold text-gray-200">AI Folio</h1>
-        <p className="text-center text-gray-600">© 2025 AI Folio. All rights reserved.</p>
+        className="text-center text-[80px] md:text-[200px] font-bold text-gray-200">GitExhibit</h1>
+        <p className="text-center text-gray-600">© 2025 Exhibit AI. All rights reserved.</p>
       </footer>
     </div>
   );
