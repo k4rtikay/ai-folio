cf21f9af1e9942848bd8dcd787848510de8502f6
k4rtikay <katt1bh4i@gmail.com>
2025-12-26 17:17:05 +0000

    feat: add dark mode functionality to the toggle

--- src/components/portfolio/templates/standard/header.tsx
+++ src/components/portfolio/templates/standard/header.tsx
@@ -1,6 +1,6 @@
 import { Button } from "@/components/ui/button";
 import { Sun } from "lucide-react";
-import { UserProfile } from "@/lib/github";
+import { useTheme } from "next-themes";
 
 interface HeaderProps {
     name?: string | null;
@@ -8,11 +8,25 @@ interface HeaderProps {
 
 export default function Header({ name }: HeaderProps) {
 
+    const { theme, setTheme } = useTheme();
+
+    const handleTheming = () => {
+        if (theme === "dark") {
+            setTheme("light");
+        } else {
+            setTheme("dark");
+        }
+    };
+
     return (
         <header className="w-full flex justify-between items-center px-4 py-2 border-b border-gray-200 dark:border-gray-800">
             <p className="text-lg font-semibold">{name}</p>
             <div>
-                <Button variant="ghost" size="icon-sm">
+                <Button
+                    variant="ghost"
+                    size="icon-sm"
+                    onClick={handleTheming}
+                >
                     <Sun />
                 </Button>
             </div>
