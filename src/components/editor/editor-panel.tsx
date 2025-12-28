097eac6e3831a5fc825f21e5ee070112ff9938b1
k4rtikay <katt1bh4i@gmail.com>
2025-12-28 13:06:33 +0000

    feat: move the action buttons from preview to edit panel

--- src/components/editor/editor-panel.tsx
+++ src/components/editor/editor-panel.tsx
@@ -3,6 +3,8 @@
 import { useState } from "react";
 import { Button } from "../ui/button";
 import { Sidebar } from "lucide-react";
+import { Forward, MonitorSmartphone } from "lucide-react";
+import { cn } from "@/lib/utils";
 
 interface EditorPanelProps {
     username: string;
@@ -14,16 +16,34 @@ export default function EditorPanel({ username, open, onToggle }: EditorPanelPro
     return (
         <div className="h-full w-full bg-[#26262B] text-[#F2F4F7] px-2 py-3 flex flex-col items-center">
             <div className="w-full flex justify-between items-center">
-                {open && <h1>Editor Panel</h1>}
-                <Button
-                    variant={"outline"}
-                    size={"icon-sm"}
-                    onClick={() => onToggle()}
-                    className="bg-[#26262B] hover:bg-gray-800 border-none"
-                >
-                    <Sidebar className="w-4 h-4 text-[#F2F4F7]" />
-                </Button>
+                {open &&
+                    <Button variant={"outline"} size={"sm"} className="bg-[#313136] hover:bg-gray-800 hover:text-[#F2F4F7] border-none">
+                        <span className="flex gap-2 items-center w-full">
+                            <p className="text-xs">@{username}</p>
+                        </span>
+                    </Button>
+                }
+
+                <div className={cn("items-center", open ? "flex gap-2" : "flex flex-col-reverse gap-2")}>
+                    <Button variant={"outline"} size={"icon-sm"} className="bg-[#313136] hover:bg-gray-800 hover:text-[#F2F4F7] border-none">
+                        <Forward className="w-4 h-4 text-[#F2F4F7]"></Forward>
+                    </Button>
+
+                    <Button variant={"outline"} size={"icon-sm"} className="bg-[#313136] hover:bg-gray-800 hover:text-[#F2F4F7] border-none">
+                        <MonitorSmartphone className="w-4 h-4 text-[#F2F4F7]"></MonitorSmartphone>
+                    </Button>
+
+                    <Button
+                        variant={"outline"}
+                        size={"icon-sm"}
+                        onClick={() => onToggle()}
+                        className="bg-[#26262B] hover:bg-gray-800 border-none"
+                    >
+                        <Sidebar className="w-4 h-4 text-[#F2F4F7]" />
+                    </Button>
+                </div>
             </div>
+
         </div >
     );
 }
--- src/components/portfolio/portfolio-preview.tsx
+++ src/components/portfolio/portfolio-preview.tsx
@@ -1,9 +1,12 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import { Forward, MonitorSmartphone } from "lucide-react";
 import StandardTemplate from "./templates/standard/standard-layout";
 import { cn } from "@/lib/utils";
 import { AIFolio } from "@/schemas/aifolio.schema";
 import { Repo, UserProfile } from "@/lib/github";
+import { useState } from "react";
 
 interface PortfolioPreviewProps {
     username: string;
@@ -13,6 +16,9 @@ interface PortfolioPreviewProps {
 }
 
 export default function PortfolioPreview({ username, portfolio, profile, repos }: PortfolioPreviewProps) {
+
+    const [isMobile, setIsMobile] = useState<boolean>(false);
+
     return (
         <div className="flex flex-col h-full w-full m-1 bg-[#1D1D21] rounded-xl overflow-hidden">
             <header className="flex justify-between items-center p-2 text-[#F2F4F7] text-sm tracking-wide">
@@ -23,20 +29,6 @@ export default function PortfolioPreview({ username, portfolio, profile, repos }
                 </div>
 
                 <h1 className="font-semibold">Preview</h1>
-
-                <div className="flex  gap-1 items-center">
-                    <Button variant={"outline"} size={"icon-sm"} className="bg-[#313136] hover:bg-gray-800 hover:text-[#F2F4F7] border-none">
-                        <Forward className="w-4 h-4 text-[#F2F4F7]"></Forward>
-                    </Button>
-                    <Button variant={"outline"} size={"icon-sm"} className="bg-[#313136] hover:bg-gray-800 hover:text-[#F2F4F7] border-none">
-                        <MonitorSmartphone className="w-4 h-4 text-[#F2F4F7]"></MonitorSmartphone>
-                    </Button>
-                    <Button variant={"outline"} size={"sm"} className="bg-[#313136] hover:bg-gray-800 hover:text-[#F2F4F7] border-none">
-                        <span className="flex gap-2 items-center w-full">
-                            <p className="text-xs">@{username}</p>
-                        </span>
-                    </Button>
-                </div>
             </header>
 
             <div className="h-full w-full overflow-y-auto px-1 rounded-xl">
