ff16669975294f49f79350483a99e37407836941
k4rtikay <katt1bh4i@gmail.com>
2025-12-29 13:54:07 +0000

    feat: create state for handling reset

--- src/store/use-portfolio-state.ts
+++ src/store/use-portfolio-state.ts
@@ -5,22 +5,30 @@ import { UserProfile } from "@/lib/github";
 
 interface PortfolioState {
     portfolio: AIFolio | null;
+    originalPortfolio: AIFolio | null;
     repos: Repo[];
     profile: UserProfile | null;
+    originalProfile: UserProfile | null;
     setPortfolio: (portfolio: AIFolio) => void;
     setRepos: (repos: Repo[]) => void;
     setProfile: (profile: UserProfile) => void;
     updatePortfolioField: (field: keyof AIFolio, value: AIFolio[keyof AIFolio]) => void;
     updateProfileField: (field: keyof UserProfile, value: UserProfile[keyof UserProfile]) => void;
+    resetPortfolio: () => void;
+    resetProfile: () => void;
+    resetPortfolioField: (field: keyof AIFolio ) => void;
+    resetProfileField: (field: keyof UserProfile) => void;
 }
 
 export const usePortfolioStore = create<PortfolioState>((set) => ({
     portfolio: null,
     repos: [],
     profile: null,
-    setPortfolio: (portfolio) => set({ portfolio }),
+    originalPortfolio: null,
+    originalProfile: null,
+    setPortfolio: (portfolio) => set({ portfolio, originalPortfolio: portfolio }),
     setRepos: (repos) => set({ repos }),
-    setProfile: (profile) => set({ profile }),
+    setProfile: (profile) => set({ profile, originalProfile: profile }),
     updatePortfolioField: (field, value) => {
         set((state)=>{
             if(!state.portfolio) return {};
@@ -37,5 +45,23 @@ export const usePortfolioStore = create<PortfolioState>((set) => ({
             }
         })
     },
+    resetPortfolio: () => set((state) => ({ portfolio: state.originalPortfolio })),
+    resetProfile: () => set((state) => ({ profile: state.originalProfile })),
+    resetPortfolioField: (field) => {
+        set((state) => {
+            if (!state.portfolio) return {};
+            return {
+                portfolio: { ...state.portfolio, [field]: state.originalPortfolio?.[field] }
+            }
+        })
+    },
+    resetProfileField: (field) => {
+        set((state) => {
+            if (!state.profile) return {};
+            return {
+                profile: { ...state.profile, [field]: state.originalProfile?.[field] }
+            }
+        })
+    }
 }));
  
