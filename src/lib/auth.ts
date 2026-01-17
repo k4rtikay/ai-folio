952209f4e651f9df72c9ee39e1e8456d2ffbd812
k4rtikay <katt1bh4i@gmail.com>
2026-01-17 12:35:57 +0000

    feat: setup github oauth

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -11,8 +11,8 @@ export const auth = betterAuth({
     providers: [
         {
             provider: "github",
-            clientId: process.env.GITHUB_CLIENT_ID!,    
-            clientSecret: process.env.GITHUB_CLIENT_SECRET!,
+            clientId: process.env.GITHUB_CLIENT_ID as string,    
+            clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
         },
     ],
 });
