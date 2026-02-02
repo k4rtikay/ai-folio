2339aaf70f16e1ded8bc9cc40fa6052a4b251229
k4rtikay <katt1bh4i@gmail.com>
2026-02-02 12:11:21 +0000

    style: landing page footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import LandingHero from "@/components/shared/landing-hero";
 export default async function HomePage() {
 
   return (
-    <div className="min-h-screen bg- flex flex-col justify-between">
+    <div className="flex flex-col justify-between">
       <header className="flex justify-between px-4 py-2">
         <h1>GitExhibit</h1>
         <span className="flex gap-2">
--- src/components/shared/landing-footer.tsx
+++ src/components/shared/landing-footer.tsx
@@ -2,28 +2,28 @@ import { UsernameForm } from "./username-form";
 
 export default function LandingFooter() {
     return (
-        <footer className="flex flex-col gap-2 w-full bg-gray-50 py-2">
+        <footer className="flex flex-col gap-2 w-full bg-neutral-900 py-2">
         <div className="flex justify-between px-4 py-2">
           <div className="grid grid-cols-2 gap-8 md:grid-cols-4 w-full">
             <div className="flex flex-col gap-2">
-              <h3 className="text-sm font-semibold text-gray-900">Product</h3>
-              <a href="#" className="text-sm text-gray-600 hover:text-gray-900">Features</a>
-              <a href="#" className="text-sm text-gray-600 hover:text-gray-900">Pricing</a>
-              <a href="#" className="text-sm text-gray-600 hover:text-gray-900">Security</a>
+              <h3 className="text-sm font-semibold text-neutral-400">Product</h3>
+              <a href="#" className="w-fit text-sm text-neutral-500 hover:text-neutral-300 transition duration-125">Features</a>
+              <a href="#" className="w-fit text-sm text-neutral-500 hover:text-neutral-300 transition duration-125">Pricing</a>
+              <a href="#" className="w-fit text-sm text-neutral-500 hover:text-neutral-300 transition duration-125">Security</a>
             </div>
 
             <div className="flex flex-col gap-2">
-              <h3 className="text-sm font-semibold text-gray-900">Resources</h3>
-              <a href="#" className="text-sm text-gray-600 hover:text-gray-900">Docs</a>
-              <a href="#" className="text-sm text-gray-600 hover:text-gray-900">Github</a>
-              <a href="#" className="text-sm text-gray-600 hover:text-gray-900">Blog</a>
+              <h3 className="text-sm font-semibold text-neutral-400">Resources</h3>
+              <a href="#" className="w-fit text-sm text-neutral-500 hover:text-neutral-300 transition duration-125">Docs</a>
+              <a href="#" className="w-fit text-sm text-neutral-500 hover:text-neutral-300 transition duration-125">Github</a>
+              <a href="#" className="w-fit text-sm text-neutral-500 hover:text-neutral-300 transition duration-125">Blog</a>
             </div>
 
             <div className="flex flex-col gap-2">
-              <h3 className="text-sm font-semibold text-gray-900">Contact</h3>
-              <a href="#" className="text-sm text-gray-600 hover:text-gray-900">Support</a>
-              <a href="#" className="text-sm text-gray-600 hover:text-gray-900">Contribute</a>
-              <a href="#" className="text-sm text-gray-600 hover:text-gray-900">Feedback</a>
+              <h3 className="text-sm font-semibold text-neutral-400">Contact</h3>
+              <a href="#" className="w-fit text-sm text-neutral-500 hover:text-neutral-300 transition duration-125">Support</a>
+              <a href="#" className="w-fit text-sm text-neutral-500 hover:text-neutral-300 transition duration-125">Contribute</a>
+              <a href="#" className="w-fit text-sm text-neutral-500 hover:text-neutral-300 transition duration-125">Feedback</a>
             </div>
 
             <UsernameForm />
@@ -31,8 +31,8 @@ export default function LandingFooter() {
           </div>
         </div>
         <h1
-        className="text-center text-[80px] md:text-[200px] font-bold text-gray-200 select-none">GitExhibit</h1>
-        <p className="text-center text-gray-600">© 2025 Exhibit AI. All rights reserved.</p>
+        className="text-center text-[80px] md:text-[200px] font-bold text-black/50 text-shadow-lg text-shadow-neutral-600/12 select-none">GitExhibit</h1>
+        <p className="text-center text-neutral-600">© 2026 GitExhibit. All rights reserved.</p>
       </footer>
     );
 }
