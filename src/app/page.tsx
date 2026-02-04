25bc6a96767837c9861dac3791381ea4ee93b66f
k4rtikay <katt1bh4i@gmail.com>
2026-02-04 14:56:43 +0000

    style: add icon and style button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,15 +2,16 @@ import { UsernameForm } from "@/components/shared/username-form";
 import LoginButton from "@/components/shared/login-button";
 import LandingFooter from "@/components/shared/landing-footer";
 import LandingHero from "@/components/shared/landing-hero";
+import { Github } from "lucide-react";
 
 export default async function HomePage() {
 
   return (
     <div className="flex flex-col justify-between">
-      <header className="flex justify-between px-4 py-2">
-        <h1>GitXhibit</h1>
-        <span className="flex gap-2">
-          <button>Github</button> 
+      <header className="flex justify-between px-4 pt-2 items-center">
+        <h1 className="font-semibold">GitXhibit</h1>
+        <span className="flex gap-8 items-center">
+          <a href="https://github.com/k4rtikay/gitxhibit" className="hover:text-primary transition-colors"><Github/></a> 
           <LoginButton/>
         </span>
       </header>
