dd5abbcc418c2aeac69d4d42a061141543c01ad0
k4rtikay <katt1bh4i@gmail.com>
2026-02-04 14:55:10 +0000

    fix: remove loading state

--- src/components/shared/login-button.tsx
+++ src/components/shared/login-button.tsx
@@ -6,44 +6,35 @@ import { useRouter } from "next/navigation";
 
 export default function LoginButton() {
 
-    const {
-        data: session,
-        isPending,
-        error,
-        refetch
-    } = authClient.useSession()
-
+    const { data: session } = authClient.useSession()
     const router = useRouter()
 
-    return (
-
-        isPending ? (
-            <Button>Loading...</Button>
-        ) : (
-            session ? (
-                <Button
-                    onClick={async () => await authClient.signOut({
-                        fetchOptions: {
-                            onSuccess: () => {
-                                router.refresh()
-                            }
-                        }
-                    })}>
-                    Logout
-                </Button>
-            ) : (
-                <Button
-                    onClick={async () => await authClient.signIn.social({
-                        provider: "github",
-                        fetchOptions: {
-                            onError: (ctx) => {
-                                console.log(ctx.error)
-                            }
-                        }
-                    })}>
-                    Login
-                </Button>
-            )
-        )
+    return session ? (
+        <Button
+            variant="outline"
+            size="sm"
+            onClick={async () => await authClient.signOut({
+                fetchOptions: {
+                    onSuccess: () => {
+                        router.refresh()
+                    }
+                }
+            })}>
+            Logout
+        </Button>
+    ) : (
+        <Button
+            variant="outline"
+            size="sm"
+            onClick={async () => await authClient.signIn.social({
+                provider: "github",
+                fetchOptions: {
+                    onError: (ctx) => {
+                        console.log(ctx.error)
+                    }
+                }
+            })}>
+            Login
+        </Button>
     )
 }
