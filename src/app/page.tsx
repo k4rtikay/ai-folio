dbf16d3e8da301d0e28fedc0e408c9811d727e17
k4rtikay <katt1bh4i@gmail.com>
2026-07-03 13:44:51 +0000

    feat: create feature section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,20 +9,30 @@ export default async function HomePage() {
             <div className="min-h-screen flex flex-col">
                 <LandingHeader />
                 <main className="flex flex-col items-center p-6 md:px-24 md:pt-16 md:pb-0 flex-1 mt-24">
-                    <div className="text-center max-w-xl mt-8 mb-6">
-                        <h1 className="text-5xl font-semibold leading-[1.1] tracking-tight font-display px-4">
-                            From GitHub to Portfolio. Instantly.
-                        </h1>
-                        <p className="mt-4 text-lg text-muted-foreground md:leading-relaxed px-4 md:px-0">
-                            Showcase your work with stunning, customizable dev
-                            portfolio templates and let AI handle the copy.
+                    <section className="flex flex-col items-center justify-center">
+                        <div className="text-center max-w-xl mt-8 mb-6">
+                            <h1 className="text-5xl font-semibold leading-[1.1] tracking-tight font-display px-4">
+                                From GitHub to Portfolio. Instantly.
+                            </h1>
+                            <p className="mt-4 text-lg text-muted-foreground md:leading-relaxed px-4 md:px-0">
+                                Showcase your work with stunning, customizable
+                                dev portfolio templates and let AI handle the
+                                copy.
+                            </p>
+                        </div>
+                        <UsernameForm />
+                        <p className="text-center text-muted-foreground text-xs leading-relaxed mt-4">
+                            Read-only access. We don&apos;t touch your private
+                            data.
                         </p>
-                    </div>
-                    <UsernameForm />
-                    <p className="text-center text-muted-foreground text-xs leading-relaxed mt-4">
-                        Read-only access. We don&apos;t touch your private data.
-                    </p>
-                    <LandingHero />
+                        <LandingHero />
+                    </section>
+                    <section className="flex flex-col items-center">
+                        <h2 className="mt-32 mb-4 text-center max-w-xl text-5xl font-semibold leading-[1.1] tracking-tight font-display px-4">
+                            Let your work do the talking.
+                        </h2>
+                        <p className="text-center text-muted-foreground text-xs leading-relaxed mt-4"></p>
+                    </section>
                 </main>
             </div>
             <LandingFooter />
